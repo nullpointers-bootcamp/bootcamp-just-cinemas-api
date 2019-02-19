0401c6605753fd6165c7615698680b8610c82602
Saniya Simeen <saniya@In-Saniya.local>
2019-02-19 13:01:48 +0530

    Added image names to db fields

---
+++ src/main/resources/db/migration/V5__movie_dump.sql
@@ -0,0 +1,24 @@
+insert into LANGUAGE (id, name) values (1, 'English');
+
+-- insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Iron Man 1', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING', 1, 'ironman1', 'ironman1_1, ironman1_2',
+--                     'When an industrialist is captured, he constructs a high-tech armoured suit to escape. Once he manages to escape, he decides to use his suit to fight against evil forces and save the world.');
+-- insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Iron Man 2', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING', 1, 'ironman2', 'ironman2_1, ironman2_2',
+--                     'Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies.');
+-- insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Iron Man 3', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING', 1, 'ironman3', 'ironman3_1, ironman3_2',
+--                     'Tony Stark encounters a formidable foe called the Mandarin. After failing to defeat his enemy, he embarks on a journey with relentless consequences and a suit that ceases to exist.');
+-- insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Avengers', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING', 1, 'avengers', 'avengers_1, avengers_2',
+--                     'In the film, Nick Fury, director of the spy agency S.H.I.E.L.D., recruits Tony Stark, Steve Rogers, Bruce Banner, and Thor to form a team that must stop Thors brother Loki from subjugating Earth.');
+-- insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Thor', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING', 1, 'thor', 'thor_1, thor_2',
+--                     'Thor is exiled by his father Odin, the King of Asgard, to the Earth to live among mortals. When he lands on Earth, his trusted weapon Mjolnir is discovered and captured by S.H.I.E.L.D.');
+-- insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Age of Ultron', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING', 1, 'ageofultron', 'ageofultron_1, ageofultron_2',
+--                     'Tony Stark builds an artificial intelligence system named Ultron with the help of Bruce Banner. When the sentient Ultron makes plans to wipe out the human race, the Avengers set out to stop him.');
+
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Kabali', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING',2,'kabali','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Theri', 'RDX, Dolby Atmos, SUB', 'NOW_SHOWING',2,'theri','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Banjo', 'RDX 3D', 'NOW_SHOWING',2,'banjo','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Suicide Squad', 'Dolby Atmos, RDX 3D, SUB', 'NOW_SHOWING',2,'suicide-squad','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Namadhu', 'RDX, SUB', 'NOW_SHOWING',2,'namadhu','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Pelli Chupulu', 'RDX, SUB', 'NOW_SHOWING',2,'pelli-chupulu','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Pink', 'RDX, SUB', 'NOW_SHOWING',2,'pink','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Remo', 'Dolby Atmos, RDX, SUB', 'NOW_SHOWING',2,'remo','','');
+insert into MOVIE (name, experiences, listing_type, lang, image_name, stills, synopsis) values ('Inferno', 'Dolby Atmos, RDX, SUB', 'NOW_SHOWING',2,'inferno','','');
